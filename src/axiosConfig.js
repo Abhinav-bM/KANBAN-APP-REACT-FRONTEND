1eab2288cdcc78d90c1b13b49d9b5e2fdb4ac52e
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-07-18 16:25:17 +0530

    update

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://kanban-app-react-backend.vercel.app",
 });
 
 export default instance;
